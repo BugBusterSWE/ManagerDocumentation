3beefb6441d92e8bc3dd3c5e383491a682aaa03d
korut94 <andreamnt94@gmail.com>
2016-03-01 21:25:46 +0100

    Remove part of React

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
-var React = require( 'react' );
-var ReactDom = require( 'react-dom' );
-
-ReactDOM.render(
-	<h1>Hello, world!</h1>,
-    document.getElementById('example')
-);
-
 function start() {
     console.log( "Start" );
 }
