fb8ed6baa5b32550fbe6efc6d619d26474615ad2
korut94 <andreamnt94@gmail.com>
2016-03-01 21:05:05 +0100

    Print log message

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var React = require( 'react' );
+var ReactDom = require( 'react-dom' );
+
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
+
+console.log( "Render window" );
