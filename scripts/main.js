f7df0ea32f0c0ec0fed608f7aa7ae4d29ea67603
korut94 <andreamnt94@gmail.com>
2016-03-02 20:51:46 +0100

    Use extented module

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 function start() {
     const socket = require( './socket_plugin' );
-    socket.run( "gulpease", [ "/home/amantova/Projects/documentation/RR/PianoDiProgetto/" ], function ( num ) {
+    socket.run( "git@github.com:BugBusterSWE/ManagerDocumentation.git::gulpease", [ "/home/amantova/Projects/documentation/RR/PianoDiProgetto/" ], function ( num ) {
 	document.getElementById( "example" ).innerHTML = num;
     });
 }
