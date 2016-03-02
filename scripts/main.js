fb5ea3ebefacab5983bb4c69c2d2bcdf4946a432
korut94 <andreamnt94@gmail.com>
2016-03-02 20:38:06 +0100

    Correct syntax error

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,5 @@ function start() {
     const socket = require( './socket_plugin' );
     socket.run( "gulpease", [ "/home/amantova/Projects/documentation/RR/PianoDiProgetto" ], function ( num ) {
 	document.getElementById( "example" ).innerHTML = num;
-    }
+    });
 }
