cfe483e42e9b88939c81afd50ef8fee66f25449b
korut94 <andreamnt94@gmail.com>
2016-04-10 12:26:10 +0200

    Add comunent way gui

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
-function start() {
+// La path deve essere assoluta!
+function gulpease( doc ) {
     const socket = require( './socket_plugin' );
-    socket.run( "git@github.com:BugBusterSWE/ManagerDocumentation.git::gulpease", [ "/home/amantova/Projects/documentation/RR/PianoDiProgetto/" ], function ( num ) {
-	document.getElementById( "example" ).innerHTML = num;
+    socket.run( 
+        "git@github.com:BugBusterSWE/ManagerDocumentation.git::gulpease", 
+        [ doc ], 
+        function ( num ) {
+	       document.getElementById( doc ).innerHTML = num;
     });
+}
+
+function addDocument() {
+    var input_path = document.getElementById( "input_document" );
+    var path = input_path.value;
+    
+    if ( path !== "" ) {
+       input_path.value = "";
+    
+        var documents = document.getElementById( "documentSet" );
+
+        var doc = document.createElement( "li" );
+        doc.innerHTML = 
+            `<button type='button' onclick='gulpease( "${path}" )'>
+            Calculate</button> ${path} <strong id=${path}>0</strong>`;
+
+        documents.appendChild( doc ); 
+    }
 }
