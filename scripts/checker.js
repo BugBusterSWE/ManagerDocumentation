d5fdefad69cc2365d0bd904a10adbfd3c76e3f7f
Andrea Mantovani <andreamnt94@gmail.com>
2016-04-28 18:21:55 +0200

    Append final text without error in result

--- scripts/checker.js
+++ scripts/checker.js
@@ -1,68 +1,73 @@
 function getSelect(array) {
-	var select = document.createElement("select");
-	var option = undefined;	
+    var select = document.createElement("select");
+    var option = undefined;	
 
-	array.forEach( function (value) {
-		option = document.createElement("option");
-		option.value = value;
-		option.text = value;
-		
-		select.appendChild(option);
-	});
+    array.forEach( function (value) {
+	option = document.createElement("option");
+	option.value = value;
+	option.text = value;
+	
+	select.appendChild(option);
+    });
 
-	return select;
+    return select;
 }
 
 function solve() {
-	var targets = ["provaro","pass"];
-	var regWord = /\w+/g;
+    var targets = ["provaro","pass"];
+    var regWord = /\w+/g;
 
-	var controls = [];
+    var controls = [];
 
-	var text = document.getElementById("prova").innerHTML;
-	var words = text.match(regWord);
-	console.log(words);
-	var before = "";
+    var text = document.getElementById("prova").innerHTML;
+    var words = text.match(regWord);
+    console.log(words);
+    var before = "";
 
-	var start = 0;
+    var start = 0;
 
-	words.forEach( function (word) {
-		var pullTarget = [];
+    words.forEach( function (word) {
+	var pullTarget = [];
 
-		targets.forEach( function (target) {
-			var distance = getEditDistance(word.toLowerCase(),target);
+	targets.forEach( function (target) {
+	    var distance = getEditDistance(word.toLowerCase(),target);
 
-			if (distance > 0 && distance <= 2) {
-				pullTarget.push(word,target);
-			}
-		});
-
-		if (pullTarget.length > 0) {
-			var chunk = {
-				end: text.indexOf(word,start),
-				mistake: pullTarget			
-			};
-
-			start = chunk.end + word.length;
-		
-			controls.push(chunk);
-		}
+	    if (distance > 0 && distance <= 2) {
+		pullTarget.push(word,target);
+	    }
 	});
 
-	start = 0;
-	var result = "";
-	var result = document.getElementById("check");
+	if (pullTarget.length > 0) {
+	    var chunk = {
+		end: text.indexOf(word,start),
+		mistake: pullTarget			
+	    };
 
-	controls.forEach( function ( chunk ) {
-		if ( chunk.end - start > 0 ) {
-			result.innerHTML = result.innerHTML + text.substring(start,chunk.end);
-		}	
-		
-		var choice = getSelect(chunk.mistake);
-		result.appendChild(choice);
+	    start = chunk.end + word.length;
+	    
+	    controls.push(chunk);
+	}
+    });
 
-		start = chunk.end + chunk.mistake[0].length; 
-	});
+    start = 0;
+    var result = "";
+    var result = document.getElementById("check");
+
+    controls.forEach( function ( chunk ) {
+	if ( chunk.end - start > 0 ) {
+	    result.innerHTML = 
+		result.innerHTML + text.substring(start,chunk.end);
+	}	
+	
+	var choice = getSelect(chunk.mistake);
+	result.appendChild(choice);
+
+	start = chunk.end + chunk.mistake[0].length; 
+    });
+    
+    if ( start > 0 ) {
+	result.innerHTML = result.innertHTML + text.substring(start);
+    }
 }
 
 
