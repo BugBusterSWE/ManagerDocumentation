06530f14a18844ca89b38d5bf2bc54f370ab4efa
Andrea Mantovani <andreamnt94@gmail.com>
2016-05-02 16:59:31 +0200

    Add method to substiture the incorret words with the words matched in the dictionary

--- scripts/checker.js
+++ scripts/checker.js
@@ -40,22 +40,46 @@ function selectSource() {
 /**
  * Return the select html element. The inside options will be set with the array values.
  * @param array {Array} Array of options
+ * @param tag {number}  End position of word
  * @return {HTMLSelect} A select element with the options of the array
  */
-function getSelect(array) {
+function getSelect(array,tag) {
     var select = document.createElement("select");
+    select.setAttribute("tag",tag);
     var option = undefined;	
 
     array.forEach( function (value) {
 	option = document.createElement("option");
-	option.value = value;
 	option.text = value;
-	
+	option.value = value;
 	select.appendChild(option);
     });
 
     return select;
 }
+
+/**
+ * Return the string with the correction selected.
+ * @return {string} Correct text
+ */
+function correct() {
+    var list = document.querySelectorAll("div#check > select");
+    var buffer = "";
+    var start = 0;
+
+    for ( var select of list ) {
+	var tag = select.getAttribute("tag");
+	var mistake = select.options[0].value;
+	var replacement = select.options[select.selectedIndex].value;
+	
+	// Substiture the word mistake with the work replacement.
+	buffer += source.substring(start,tag).split(mistake).join(replacement);
+	start = tag;
+    }
+
+    console.log(buffer);
+}
+
 
 function solve(text) {
     var regWord = /\w+/g;
@@ -68,24 +92,30 @@ function solve(text) {
     var start = 0;
 
     words.forEach( function (word) {
-	var pullTarget = [];
+	// Append the uncorrect word in the array assure that it is the first 
+	// option in the select element
+	var pullTarget = [word];
 
 	dictionary.forEach( function (target) {
 	    var distance = getEditDistance(word.toLowerCase(),target);
 
+	    // Append any likely matchs
 	    if (distance > 0 && distance <= 2) {
-		pullTarget.push(word,target);
+		pullTarget.push(target);
 	    }
 	});
 
-	if (pullTarget.length > 0) {
+	if (pullTarget.length > 1) {
+	    var pos = source.indexOf(word,start);
+
 	    var chunk = {
-		end: source.indexOf(word,start),
+		end: pos,
+		tag: pos + word.length,
 		mistake: pullTarget			
 	    };
 
 	    start = chunk.end + word.length;
-	    
+
 	    controls.push(chunk);
 	}
     });
@@ -99,7 +129,7 @@ function solve(text) {
 		result.innerHTML + source.substring(start,chunk.end);
 	}	
 
-	var choice = getSelect(chunk.mistake);
+	var choice = getSelect(chunk.mistake,chunk.tag);
 	result.appendChild(choice);
 
 	start = chunk.end + chunk.mistake[0].length; 
